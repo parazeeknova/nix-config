ce03c35c850a1efa5c9dfeef58bcb77f90850567
Harsh Sahu <yesh8harsh+github@gmail.com>
2024-06-23 21:15:26 +0530

    remove build warning for configuration.nix
    Next : Add modularity

--- configuration.nix
+++ configuration.nix
@@ -46,8 +46,8 @@
 
   # Configure keymap in X11
   services.xserver = {
-    layout = "us";
-    xkbVariant = "";
+    xkb.layout = "us";
+    xkb.variant = "";
   };
 
   # Enable sound with pipewire.
